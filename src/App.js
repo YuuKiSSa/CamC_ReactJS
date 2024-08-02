0a1ddf54f791c849000380f7fd8ca1e9e216df6b
YuuKiSSa <e1241986@u.nus.edu>
2024-08-02 20:31:00 +0800

    add Nav and footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CameraList from "./Components/CameraList";
 import CameraDetail from "./Components/CameraDetail";
-import CameraPrice from "./Components/CameraPrice";
+import Navbar from "./Components/Page/Navbar";
+import Footer from "./Components/Page/Footer";
 import './CSS/App.css';
 
 function App() {
   return (
       <Router>
-          <Routes>
-              <Route path="/" element={<CameraList />} />
-              <Route path="/camera/:id" element={<CameraDetail />} />
-          </Routes>
+          <div className="App">
+              <Navbar />
+              <div className="content">
+                  <Routes>
+                      <Route path="/gallery" element={<CameraList />} />
+                      <Route path="/camera/:id" element={<CameraDetail />} />
+                  </Routes>
+              </div>
+              <Footer />
+          </div>
+
       </Router>
   );
 }
---
+++ src/Components/Page/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; Copyright 2024 CamC.NFT. All rights reserved.</p>
+            <p><a href="https://www.example.com">www.example.com</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
