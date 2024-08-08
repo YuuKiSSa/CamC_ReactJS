dcd0f9283b33835c032290f994654a053ca032cb
YuuKiSSa <e1241986@u.nus.edu>
2024-08-08 21:15:00 +0800

    admin page and edit page 7/8/2024

---
+++ src/Components/AdminHome.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../CSS/AdminHome.css";
+import {Link, useNavigate} from "react-router-dom";
+
+function AdminCameraList() {
+    const [cameras, setCameras] = useState([]);
+    const navigate = useNavigate();
+    const [currentPage, setCurrentPage] = useState(1);
+    const PAGE_SIZE = 10;
+
+    useEffect(() => {
+        const fetchCameras = async () => {
+            try {
+                const response = await axios.get("http://localhost:8080/api/list");
+                const adminResponse = await axios.get("http://localhost:8080/api/current-user", { withCredentials: true });
+                setCameras(response.data.cameras);
+                if (adminResponse.data.id !== 'admin'){
+                    window.alert("You can't access the admin page!");
+                    navigate("/login");
+                }
+            } catch (error) {
+                console.error(error);
+                window.alert("You can't access the admin page!");
+                navigate("/login");
+            }
+        };
+        fetchCameras();
+    }, [navigate]);
+
+    const indexOfLastCamera = currentPage * PAGE_SIZE;
+    const indexOfFirstCamera = indexOfLastCamera - PAGE_SIZE;
+    const currentCameras = cameras.slice(indexOfFirstCamera, indexOfLastCamera);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const handlePrevious = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNext = () => {
+        if (currentPage < Math.ceil(cameras.length / PAGE_SIZE)) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(cameras.length / PAGE_SIZE); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <div className="admin-camera-list">
+            <h1>Camera List</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Image</th>
+                    <th>Brand</th>
+                    <th>Model</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                {currentCameras.map(camera => (
+                    <tr key={camera.id}>
+                        <td><Link to={`/edit-camera/${camera.id}`}>{camera.id}</Link></td>
+                        <td><img src={camera.imageUrl} alt={camera.model}/></td>
+                        <td>{camera.brand}</td>
+                        <td>{camera.model}</td>
+                        <td>{camera.latestPrice}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button
+                    onClick={handlePrevious}
+                    className="pagination-button"
+                    disabled={currentPage === 1}
+                >
+                    &lt; Prev
+                </button>
+                {pageNumbers.map(number => (
+                    <button
+                        key={number}
+                        onClick={() => paginate(number)}
+                        className={`pagination-button ${number === currentPage ? 'active' : ''}`}
+                    >
+                        {number}
+                    </button>
+                ))}
+                <button
+                    onClick={handleNext}
+                    className="pagination-button"
+                    disabled={currentPage === pageNumbers.length}
+                >
+                    Next &gt;
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default AdminCameraList;
---
+++ src/Components/EditCamera.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import "../CSS/EditCamera.css";
+
+const EditCamera = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [cameraDetails, setCameraDetails] = useState({
+        brand: "",
+        model: "",
+        category: "",
+        description: "",
+        releaseTime: "",
+        initialPrice: 0,
+        effectivePixel: 0,
+        iso: 0,
+        focusPoint: 0,
+        continuousShot: 0,
+        videoResolution: 0,
+        videoRate: 0
+    });
+
+    useEffect(() => {
+        const fetchCameraDetails = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/camera/${id}`);
+                setCameraDetails(response.data);
+            } catch (error) {
+                console.error("Failed to fetch camera details:", error);
+            }
+        };
+        fetchCameraDetails();
+    }, [id]);
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setCameraDetails((prevDetails) => ({
+            ...prevDetails,
+            [id]: value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.put(`http://localhost:8080/api/update-camera/${id}`, cameraDetails, {withCredentials: true});
+            navigate("/admin-home");
+        } catch (error) {
+            console.error("Failed to update camera details:", error);
+        }
+    };
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        try{
+            await axios.delete(`http://localhost:8080/api/delete-camera/${id}`, {withCredentials: true});
+            navigate("/admin-home");
+        }catch (error){
+            console.error("Failed to delete camera details:", error);
+        }
+    };
+
+    return (
+        <div className="edit-camera">
+            <h1>Edit Camera Details</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="brand">Brand</label>
+                <br/>
+                <select id="brand" onChange={handleChange} value={cameraDetails.brand}>
+                    <option>Canon</option>
+                    <option>Nikon</option>
+                    <option>Sony</option>
+                </select>
+                <label htmlFor="model">Model</label>
+                <input id="model" onChange={handleChange} type="text" value={cameraDetails.model}/>
+                <label htmlFor="category">Category</label>
+                <select id="category" onChange={handleChange} value={cameraDetails.category}>
+                    <option>SLR</option>
+                    <option>DC</option>
+                    <option>MIC</option>
+                </select>
+                <label htmlFor="releaseTime">Release Time</label>
+                <input id="releaseTime" onChange={handleChange} type="date" value={cameraDetails.releaseTime}/>
+                <label htmlFor="initialPrice">Initial Price</label>
+                <input id="initialPrice" onChange={handleChange} type="number" value={cameraDetails.initialPrice}/>
+                <label htmlFor="effectivePixel">Effective Pixel</label>
+                <input id="effectivePixel" onChange={handleChange} type="number" value={cameraDetails.effectivePixel}/>
+                <label htmlFor="ISO">ISO</label>
+                <input id="ISO" onChange={handleChange} type="number" value={cameraDetails.iso}/>
+                <label htmlFor="focusPoint">Focus Point</label>
+                <input id="focusPoint" onChange={handleChange} type="number" value={cameraDetails.focusPoint}/>
+                <label htmlFor="continuousShot">Continuous Shots</label>
+                <input id="continuousShot" onChange={handleChange} type="number" value={cameraDetails.continuousShot}/>
+                <label htmlFor="videoResolution">Video Resolution</label>
+                <input id="videoResolution" onChange={handleChange} type="number" value={cameraDetails.videoResolution}/>
+                <label htmlFor="videoRate">Video Rate</label>
+                <input id="videoRate" onChange={handleChange} type="number" value={cameraDetails.videoRate}/>
+                <label htmlFor="description">Description</label>
+                <textarea id="description" onChange={handleChange} value={cameraDetails.description} rows="4" cols="50"/>
+                <button type="submit" className="btn-save">Save Changes</button>
+            </form>
+            <button className="btn-delete" onClick={handleDelete}>Delete Camera</button>
+        </div>
+    );
+};
+
+export default EditCamera;
